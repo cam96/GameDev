38fe65a40b9de4b07f3d27c3abe90acffbc1837f
cam96 <cameron0960@gmail.com>
2013-11-11 21:30:43 -0600

    New Project Created

---
+++ GameDev/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Cam
+ *
+ * Created on November 11, 2013, 9:29 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
